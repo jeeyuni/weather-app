7f9779de2c79e4c964896ea7493dbef25bf19a82
Jiyun Lee <kuri12071112@gmail.com>
2024-09-14 19:50:00 +0700

    added css

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-function getWeather() {
-    const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=gfs_seamless':
-    const city = document.getElementById('city').value;
+const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=gfs_seamless';
+const state = document.getElementById('state').value;
 
 
-    async function fetchWeather() {
-        try {
-            const response = await fetch(apiUrl);
-            const data = await response.json();
-            displayWeather(data);
-        } catch (error) {
-            console.log('There was an error whilie fetching data!', error);
-        }
+async function getWeather() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        console.log('There was an error whilie fetching data!', error);
     }
+}
 
-    function displayWeather(data) {
-        //console.log(data);
-        //const weatherIcon = document.getElementById('weather-icon');
+function displayWeather(data) {
+    console.log(data);
 
-        const container = document.getElementById('imageContainer');
-        container.innerHTML = ''; //Clears previous image
-        //container.appendChild(weatherIcon);
+    
+    //const weatherIcon = document.getElementById('weather-icon');
 
-    }
+    const container = document.getElementById('imageContainer');
+    container.innerHTML = ''; //Clears previous image
+    //container.appendChild(weatherIcon);
 
 }
+
