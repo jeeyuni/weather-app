f263ebabb289f0843c74554ca090646a4d49ff52
Jiyun Lee <kuri12071112@gmail.com>
2024-09-14 18:51:28 +0700

    added displayWeather function

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=gfs_seamless";
+function getWeather() {
+    const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=gfs_seamless':
+    const city = document.getElementById('city').value;
 
-async function fetchWeather() {
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log('There was an error whilie fetching data!', error);
-  }
+
+    async function fetchWeather() {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            displayWeather(data);
+        } catch (error) {
+            console.log('There was an error whilie fetching data!', error);
+        }
+    }
+
+    function displayWeather(data) {
+        //console.log(data);
+        //const weatherIcon = document.getElementById('weather-icon');
+
+        const container = document.getElementById('imageContainer');
+        container.innerHTML = ''; //Clears previous image
+        //container.appendChild(weatherIcon);
+
+    }
+
 }
-
